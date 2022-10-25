e4442af5ba58c15312e08e850a3bfd947dc01297
Arocha13 <antoniomvapo@gmail.com>
2022-10-25 13:33:31 +0200

    first commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,20 @@
+ # Day 1 _ 30DaysOfPython Challenge 
+print(3+4)   # addition(+)
+print(3-4)   # subtraction(-)
+print(3*4)   # multiplication(*) 
+print(3%4)   # modulus(%)
+print(3/4)   # division(/)
+print(3**4)  #exponential(**)
+print(3//4)  # operator(//) 
+
+string = ("Antonio", "Arocha", "Spain", "I am enjoying 30 days of python")
+print (string) 
+ 
+list = [2, 34, 4.6, 5.79, 3, 2 + 6j, 8+4j] 
+print (list) 
+
+import numpy as np 
+a = np.array((2, 3)) 
+b = np.array((10, 8)) 
+dist = np.linalg.norm(a-b)
+print(dist)
